9db950d367b868342dc0dff78f6f3ea4ecaa2b2c
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-12-23 15:57:51 +0100

    implement pagination

--- src/resolvers/Post/postResolvers.ts
+++ src/resolvers/Post/postResolvers.ts
@@ -5,12 +5,36 @@ import {FieldError} from "../User/userResolversOutputs";
 import {ApolloRedisContext} from "../../apollo-config";
 import {PostResponse} from "./postResolversOutputs";
 import {isAuth} from "../Universal/utils";
+import {getConnection} from "typeorm";
 
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
-    posts(): Promise<Post[]> {
-        return Post.find();
+    posts(
+        @Arg('limit', () => Int,{
+            description: "For pagination." +
+                "Max number of posts. Default is 50"
+        }) limit: number,
+        @Arg('cursor', () => String, {
+            nullable: true,
+            description: "For pagination." +
+                "Offset=10 means, retrieve the 10th post. Cursor in contrast depends on the sorting" +
+                "Default sorting: (createdAt, DESC) (new first)" +
+                "The cursor accepts a string timestamp, the createdAt." +
+                "Returns all the posts after the given timestamp"
+        }) cursor: string | null,
+    ): Promise<Post[]> {
+        const realLimit = Math.min(50, limit);
+        const queryBuilder = getConnection()
+            .getRepository(Post)
+            .createQueryBuilder("posts")
+            .orderBy('"createdAt"', "DESC")
+            .take(realLimit);
+        if (cursor) {
+            queryBuilder
+                .where('"createdAt" < :cursor', {cursor: new Date(parseInt(cursor))})
+        }
+        return queryBuilder.getMany();
     }
 
     @Query(() => Post, {nullable: true})
