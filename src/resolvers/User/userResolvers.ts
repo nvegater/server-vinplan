991b5be97a0a59c49d8495d4bddc6515a5161469
P00920345 <nicolas.vegaterrazas@bdr.de>
2021-03-06 16:30:54 +0100

    save amenities and languages

--- src/resolvers/User/userResolvers.ts
+++ src/resolvers/User/userResolvers.ts
@@ -25,6 +25,8 @@ import {SQL_QUERY_GET_RESERVED_SERVICES_IDS} from "../Universal/queries";
 import {Winery} from "../../entities/Winery";
 import {WineType} from "../../entities/WineType";
 import {WineProductionType} from "../../entities/WineProductionType";
+import {WineryLanguage} from "../../entities/WineryLanguage";
+import {WineryAmenity} from "../../entities/WineryAmenity";
 
 
 @Resolver(User)
@@ -179,6 +181,26 @@ export class UserResolver {
                     productionTypes.map(async (productionTypeEntity) => {
                         await productionTypeEntity.save();
                     })
+
+                    if (wineryDataInputs.supportedLanguages && wineryDataInputs.supportedLanguages?.length > 0) {
+                        wineryDataInputs.supportedLanguages.map(async (supLan) => {
+                            const wineLanEntity = WineryLanguage.create({
+                                wineryId: winery.id,
+                                supportedLanguage: supLan
+                            });
+                            await wineLanEntity.save()
+                        });
+                    }
+
+                if (wineryDataInputs.amenities && wineryDataInputs.amenities?.length > 0) {
+                        wineryDataInputs.amenities.map(async (amenity) => {
+                            const amenityEntity = WineryAmenity.create({
+                                wineryId: winery.id,
+                                amenity: amenity
+                            });
+                            await amenityEntity.save()
+                        });
+                    }
 
                     // @ts-ignore
                     req.session.userId = user.id;
