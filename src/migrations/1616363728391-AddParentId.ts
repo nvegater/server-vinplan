8716bba63f6aa8b04be998bf4a345b6f5286c76d
P00920345 <nicolas.vegaterrazas@bdr.de>
2021-03-21 23:05:08 +0100

    correct migration script

--- src/migrations/1616363728391-AddParentId.ts
+++ src/migrations/1616363728391-AddParentId.ts
@@ -4,11 +4,10 @@ export class AddParentId1616363728391 implements MigrationInterface {
     name = 'AddParentId1616363728391'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE service ADD COLUMN parentServiceId INTEGER;`);
+        await queryRunner.query(`ALTER TABLE service ADD COLUMN parentServiceId INTEGER`);
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`COMMENT ON COLUMN "service"."updatedAt" IS NULL`);
+    public async down(_: QueryRunner): Promise<void> {
     }
 
 }
