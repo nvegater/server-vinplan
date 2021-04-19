527ecb5e148e5c793a0cf195999efdeb00bccdcc
Roberto Torres <5945088-RobertoTorresKavak@users.noreply.gitlab.com>
2021-04-18 22:21:55 -0500

    ⚡s3Utilities -se agrega la posibilidad de firmar imagenes sin requerir urls prefirmadas en consumo

--- src/utils/s3Utilities.ts
+++ src/utils/s3Utilities.ts
@@ -15,12 +15,13 @@ export async function getPresignedUrl(presignedUrl: PresignedUrlInput) {
     try {
         const {fileName} = presignedUrl
         const multimediaInfo = getMultimediaInfo(presignedUrl);
-        const key = `${multimediaInfo.prefix}/${Date.now()}`
+        const key = multimediaInfo.key;
         const expireSeconds = 60 * 5
 
         const preSignedPutUrl = await s3.getSignedUrl('putObject',{
             Bucket: `${process.env.NEXT_PUBLIC_DO_SPACES_NAME}/${key}`,
             ContentType: multimediaInfo.contentType,
+            ACL: 'public-read', 
             Expires: expireSeconds,
             Key: fileName,
         })
@@ -43,6 +44,7 @@ const getMultimediaInfo = (presignedUrl: PresignedUrlInput) => {
         const {fileName, uploadType, wineryId, userId} = presignedUrl
         const imagesTypes = ['apng', 'avif', 'gif', 'jpg', 'jpeg', 'jfif', 'pjpeg', 'pjp', 'png', 'svg', 'webp' ];
         const ext = fileName.split('.').pop() || 'badFormat';
+        let key = null
         if (!imagesTypes.includes(ext.toLowerCase())) {
             throw new Error('Tipo de archivo incorrecto');
         }
@@ -51,13 +53,17 @@ const getMultimediaInfo = (presignedUrl: PresignedUrlInput) => {
         if (uploadType == 'winerybook') {
             prefix = `winery/${wineryId}-album`;
             contentType = mime.getType(ext) || '';
+            key = `${prefix}/${Date.now()}`
         }
         if (uploadType == 'userprofilepicture'){
             prefix = `user/${userId}-pictureProfile`;
+            contentType = mime.getType(ext) || '';
+            key = `${prefix}`
         }
         return {
             prefix: prefix,
             contentType: contentType,
+            key: key,
         }
     } catch (error) {
         throw new Error(error)
