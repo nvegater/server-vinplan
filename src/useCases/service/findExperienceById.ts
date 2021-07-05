8ec719118f62079d2a43603750a3d17453cc775c
Roberto Torres <5945088-RobertoTorresKavak@users.noreply.gitlab.com>
2021-07-05 00:56:46 -0500

    ✨ findExperiencesById -se agregan los campos necesarios de la winery

--- src/useCases/service/findExperienceById.ts
+++ src/useCases/service/findExperienceById.ts
@@ -3,6 +3,14 @@ import dataServices from "../../dataServices/service";
 import ServiceImageGalleryServices from "../../dataServices/serviceImageGallery"
 import getImageCover from "../../utils/getImageCover";
 import wineryServices from "../../dataServices/winery"
+import {WineType} from "../../entities/WineType";
+import {WineProductionType} from "../../entities/WineProductionType";
+import {WineryLanguage} from "../../entities/WineryLanguage";
+import {WineryAmenity} from "../../entities/WineryAmenity";
+import WineTypeServices from "../../dataServices/wineType"
+import WineProductionTypeServices from "../../dataServices/wineProductionType"
+import WineryLanguageServices from "../../dataServices/wineryLanguage"
+import WineryAmenityServices from "../../dataServices/wineryAmenity"
 
 const findExperienceById = async (experienceId : number): Promise<FindExperienceResponse> => {
     try {
@@ -10,7 +18,17 @@ const findExperienceById = async (experienceId : number): Promise<FindExperience
             if (experienceFound != undefined) {
                 experienceFound.gallery = await ServiceImageGalleryServices.getServiceGalleryById(experienceFound?.id);
                 experienceFound.urlImageCover = await getImageCover.experience(experienceFound);
-                const wineryFound = await wineryServices.findWineryById(experienceFound.wineryId)
+                const wineryFound = await wineryServices.findWineryById(experienceFound.wineryId);
+                if (wineryFound) {
+                    const wineTypesOfWinery: WineType[] | [] = await WineTypeServices.getWineTypeByWineryId(wineryFound?.id);
+                    const prodTypesOfWinery: WineProductionType[] | [] = await WineProductionTypeServices.getProductionTypeByWineryId(wineryFound?.id);
+                    const languages: WineryLanguage[] | [] = await WineryLanguageServices.getWineryLanguageByWineryId(wineryFound?.id);
+                    const amenities: WineryAmenity[] | [] = await WineryAmenityServices.getWineryAmenityByWineryId(wineryFound?.id);
+                    wineryFound.typeProduction = prodTypesOfWinery.map((pt) => pt.productionType);
+                    wineryFound.typeWine = wineTypesOfWinery.map((wt) => wt.wineType);
+                    wineryFound.languageSupported = languages.map((lang) => lang.supportedLanguage);
+                    wineryFound.wineryAmenity = amenities.map((am) => am.amenity);
+                }
                 return {
                     service : experienceFound,
                     winery : wineryFound
