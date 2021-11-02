9a32a0ff5bedb6dabd7b68a6517f04d24e5ab34b
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2021-11-02 11:33:02 +0100

    add keycloak context to the all Services Query

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,76 @@
-import express, {Express, RequestHandler} from "express";
-import "reflect-metadata"
+import express, { Express, RequestHandler } from "express";
+import "reflect-metadata";
 // Read dotenv file and stick them as variables and throw error if an environment variable is not set
-import "dotenv-safe/config"
+import "dotenv-safe/config";
 
-import cors from "cors"
-import {corsConfig} from "./express-config";
+import cors from "cors";
+import { corsConfig } from "./express-config";
 
-import session from 'express-session';
-import connectRedis, {RedisStore} from 'connect-redis';
-import Redis, {Redis as RedisType} from 'ioredis';
-import {buildRedisSession} from "./redis-config";
+import session from "express-session";
+import connectRedis, { RedisStore } from "connect-redis";
+import Redis, { Redis as RedisType } from "ioredis";
+import { buildRedisSession } from "./redis-config";
 
-import {Connection, createConnection} from "typeorm";
-import typeOrmPostgresConfig from "./typeorm.config"
+import { Connection, createConnection } from "typeorm";
+import typeOrmPostgresConfig from "./typeorm.config";
 
-import {ApolloServer, ApolloServerExpressConfig} from "apollo-server-express";
-import {apolloKeycloakExpressContext, registerExpressServer} from "./apollo-config";
+import { ApolloServer, ApolloServerExpressConfig } from "apollo-server-express";
+import {
+  apolloKeycloakExpressContext,
+  registerExpressServer,
+} from "./apollo-config";
 
-import Keycloak from "keycloak-connect"
-
+import Keycloak from "keycloak-connect";
 
 const start_server = async () => {
+  const app: Express = express();
 
-    const app: Express = express();
+  // CORS
+  const corsRequestHandler: RequestHandler = cors(corsConfig);
+  app.use(corsRequestHandler);
 
-    // CORS
-    const corsRequestHandler: RequestHandler = cors(corsConfig);
-    app.use(corsRequestHandler)
+  // Redis
+  const redisStore: RedisStore = connectRedis(session);
+  const redisClient: RedisType = new Redis(process.env.REDIS_URL);
+  app.set("trust proxy", 1);
+  const redisRequestHandler: RequestHandler = session(
+    buildRedisSession(redisStore, redisClient)
+  );
+  app.use(redisRequestHandler);
 
-    // Redis
-    const redisStore: RedisStore = connectRedis(session)
-    const redisClient: RedisType = new Redis(process.env.REDIS_URL)
-    app.set("trust proxy", 1);
-    const redisRequestHandler: RequestHandler = session(buildRedisSession(redisStore, redisClient));
-    app.use(redisRequestHandler);
+  // TypeORM
+  const postgresConnection: Connection = await createConnection(
+    typeOrmPostgresConfig
+  );
+  // when pushing the docker image
+  // this will run everything from the migration folder
+  await postgresConnection.runMigrations();
 
-    // TypeORM
-    const postgresConnection:Connection = await createConnection(typeOrmPostgresConfig);
-    // when pushing the docker image
-    // this will run everything from the migration folder
-    await postgresConnection.runMigrations();
+  //await Post.delete({});
+  // npx typeorm migration:create -n FakePosts <----for migrations
 
-    //await Post.delete({});
-    // npx typeorm migration:create -n FakePosts <----for migrations
+  // Keycloak
+  const keycloak = new Keycloak({});
 
-    // Keycloak
-    const keycloak = new Keycloak()
+  app.use("/graphql", keycloak.middleware());
 
-    app.use("/graphql", keycloak.middleware())
+  // Apollo-redis
+  // const apolloConfig: ApolloServerExpressConfig = await apolloExpressRedisContext(redisClient);
 
+  // Apollo-Keycloak
+  const apolloKeycloakConfig: ApolloServerExpressConfig =
+    await apolloKeycloakExpressContext(keycloak);
 
-    // Apollo-redis
-    // const apolloConfig: ApolloServerExpressConfig = await apolloExpressRedisContext(redisClient);
+  new ApolloServer(apolloKeycloakConfig).applyMiddleware(
+    registerExpressServer(app)
+  );
 
-    // Apollo-Keycloak
-    const apolloKeycloakConfig: ApolloServerExpressConfig = await apolloKeycloakExpressContext(keycloak)
+  // Start server
+  app.listen(process.env.PORT, () => {
+    console.log("Server started in localhost: 4000");
+  });
+};
 
-    new ApolloServer(apolloKeycloakConfig)
-        .applyMiddleware(registerExpressServer(app))
-
-    // Start server
-    app.listen(process.env.PORT, () => {
-        console.log("Server started in localhost: 4000");
-    })
-}
-
-start_server()
-    .catch((err) => {
-        console.log(err)
-    });
+start_server().catch((err) => {
+  console.log(err);
+});
--- src/resolvers/Service/serviceResolvers.ts
+++ src/resolvers/Service/serviceResolvers.ts
@@ -1,188 +1,224 @@
-import {Arg, Ctx, Directive, Int, Mutation, Query, Resolver, UseMiddleware} from "type-graphql";
-import {Service, EventType} from "../../entities/Service";
-import {Valley} from "../../entities/Winery";
 import {
-    SalesConcentrate,
-    BookServiceResponse,
-    CreateServiceResponse,
-    ServiceResponse, PaginatedExperiences,
-    UpdateServiceResponse, FindExperienceResponse
+  Arg,
+  Ctx,
+  Directive,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from "type-graphql";
+import { Service, EventType } from "../../entities/Service";
+import { Valley } from "../../entities/Winery";
+import {
+  SalesConcentrate,
+  BookServiceResponse,
+  CreateServiceResponse,
+  ServiceResponse,
+  PaginatedExperiences,
+  UpdateServiceResponse,
+  FindExperienceResponse,
 } from "./serviceResolversOutputs";
-import {isAuth} from "../Universal/utils";
-import {ApolloKeycloakContext, ApolloRedisContext} from "../../apollo-config";
-import {ServiceImageResponse, ServiceCoverImageChangeResponse} from "./serviceResolversOutputs"
-import createDefaultImageToEvent from "../../useCases/service/createDefaultImageToEvent"
-import updateDefaultImageToEvent from "../../useCases/service/updateDefaultImageToEvent"
-import deleteDefaultImageToEvent from "../../useCases/service/deleteDefaultImageToEvent"
-import insertImage from "../../useCases/service/insertImage"
-import deleteImage from "../../useCases/service/deleteImage"
-import changeCoverPicture from "../../useCases/service/changeCoverPicture"
-import {CreateServiceInputs, ReserveServiceInputs, UpdateServiceInputs} from "./serviceResolversInputs";
+import { isAuth } from "../Universal/utils";
+import { ApolloKeycloakContext, ApolloRedisContext } from "../../apollo-config";
+import {
+  ServiceImageResponse,
+  ServiceCoverImageChangeResponse,
+} from "./serviceResolversOutputs";
+import createDefaultImageToEvent from "../../useCases/service/createDefaultImageToEvent";
+import updateDefaultImageToEvent from "../../useCases/service/updateDefaultImageToEvent";
+import deleteDefaultImageToEvent from "../../useCases/service/deleteDefaultImageToEvent";
+import insertImage from "../../useCases/service/insertImage";
+import deleteImage from "../../useCases/service/deleteImage";
+import changeCoverPicture from "../../useCases/service/changeCoverPicture";
+import {
+  CreateServiceInputs,
+  ReserveServiceInputs,
+  UpdateServiceInputs,
+} from "./serviceResolversInputs";
 import reserve from "../../useCases/service/reserve";
 import getServices from "../../useCases/service/getServices";
 import showServices from "../../useCases/service/showServices";
 import createService from "../../useCases/service/createService";
 import updateService from "../../useCases/service/updateService";
 import findExperienceById from "../../useCases/service/findExperienceById";
-import getSalesConcentrate from "../../useCases/service/salesConcentrate"
+import getSalesConcentrate from "../../useCases/service/salesConcentrate";
 
 @Resolver(Service)
 export class ServiceResolver {
-
-    @Query(() => PaginatedExperiences)
-    async allServices(
-        @Arg('limit', () => Int, {
-            description: "For pagination." +
-                "Max number of experiences. Default is 50"
-        }) limit: number,
-        @Arg('cursor', () => String, {nullable: true,
-            description: "For pagination." +
-                "Offset=10 means, retrieve the 10th experience. Cursor in contrast depends on the sorting" +
-                "Default sorting: (createdAt, DESC) (new first)" +
-                "The cursor accepts a string timestamp, the createdAt." +
-                "Returns all the experiences after the given timestamp"
-        }) cursor: string | null,
-        @Arg('experienceName', () => String, {nullable: true}) experienceName: string | null,
-        @Arg('eventType', () => [EventType], {nullable: true}) eventType: EventType[] | null,
-        @Arg('valley', () => [Valley], {nullable: true}) valley: Valley[],
-        @Arg('state', () => String, {nullable: true}) state: string | null,
-    ): Promise<PaginatedExperiences> {
-        try {
-            return await showServices(limit, cursor, experienceName, eventType, valley, state);
-        } catch (error) {
-            throw new Error(error)
-        }
-    };
-
-    @Query(() => ServiceResponse)
-    @UseMiddleware(isAuth)
-    async servicesUser(
-        @Arg('serviceIds', () => [Int]) serviceIds: number[]
-    ): Promise<ServiceResponse> {
-       return await getServices(serviceIds)
-    };
-
-    @Query(() => SalesConcentrate)
-    async salesConcentrate(
-        @Arg('paypalTransaccionId', () => String, {nullable: true, description: "if you put null or undefined" +
-        "the query will return all events registration"}) paypalTransaccionId: string | null,
-        
-    ): Promise<SalesConcentrate> {
-        return await getSalesConcentrate(paypalTransaccionId)
+  @Query(() => PaginatedExperiences)
+  async allServices(
+    @Arg("limit", () => Int, {
+      description:
+        "For pagination." + "Max number of experiences. Default is 50",
+    })
+    limit: number,
+    @Arg("cursor", () => String, {
+      nullable: true,
+      description:
+        "For pagination." +
+        "Offset=10 means, retrieve the 10th experience. Cursor in contrast depends on the sorting" +
+        "Default sorting: (createdAt, DESC) (new first)" +
+        "The cursor accepts a string timestamp, the createdAt." +
+        "Returns all the experiences after the given timestamp",
+    })
+    cursor: string | null,
+    @Arg("experienceName", () => String, { nullable: true })
+    experienceName: string | null,
+    @Arg("eventType", () => [EventType], { nullable: true })
+    eventType: EventType[] | null,
+    @Arg("valley", () => [Valley], { nullable: true }) valley: Valley[],
+    @Arg("state", () => String, { nullable: true }) state: string | null,
+    @Ctx() apolloKeycloakContext: ApolloKeycloakContext
+  ): Promise<PaginatedExperiences> {
+    try {
+      console.log(apolloKeycloakContext.kauth.isAuthenticated());
+      console.log(apolloKeycloakContext.kauth.hasRole("weno-service-provider"));
+      return await showServices(
+        limit,
+        cursor,
+        experienceName,
+        eventType,
+        valley,
+        state
+      );
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    @Mutation(() => BookServiceResponse)
-    @Directive("@auth")
-    async reserve(
-        @Arg('reserveServiceInputs') reserveServiceInputs: ReserveServiceInputs,
-        @Ctx() apolloKeycloakContext: ApolloKeycloakContext
-    ): Promise<BookServiceResponse> {
-        const {kauth} = apolloKeycloakContext
-        // @ts-ignore
-        console.log(kauth.accessToken!.content)
+  @Query(() => ServiceResponse)
+  @UseMiddleware(isAuth)
+  async servicesUser(
+    @Arg("serviceIds", () => [Int]) serviceIds: number[]
+  ): Promise<ServiceResponse> {
+    return await getServices(serviceIds);
+  }
 
-        return await reserve({userId: 1, ...reserveServiceInputs})
+  @Query(() => SalesConcentrate)
+  async salesConcentrate(
+    @Arg("paypalTransaccionId", () => String, {
+      nullable: true,
+      description:
+        "if you put null or undefined" +
+        "the query will return all events registration",
+    })
+    paypalTransaccionId: string | null
+  ): Promise<SalesConcentrate> {
+    return await getSalesConcentrate(paypalTransaccionId);
+  }
 
+  @Mutation(() => BookServiceResponse)
+  @Directive("@auth")
+  async reserve(
+    @Arg("reserveServiceInputs") reserveServiceInputs: ReserveServiceInputs,
+    @Ctx() apolloKeycloakContext: ApolloKeycloakContext
+  ): Promise<BookServiceResponse> {
+    const { kauth } = apolloKeycloakContext;
+    // @ts-ignore
+    console.log(kauth.accessToken!.content);
+
+    return await reserve({ userId: 1, ...reserveServiceInputs });
+  }
+
+  @Mutation(() => CreateServiceResponse)
+  @UseMiddleware(isAuth)
+  async createService(
+    @Arg("createServiceInputs") createServiceInputs: CreateServiceInputs,
+    @Ctx() { req }: ApolloRedisContext
+  ): Promise<CreateServiceResponse> {
+    // @ts-ignore
+    const { userId } = req.session;
+    // validate winery
+    return await createService(createServiceInputs, userId);
+  }
+
+  @Query(() => FindExperienceResponse)
+  async findExperienceById(
+    @Arg("experienceId", () => Int) experienceId: number
+  ): Promise<FindExperienceResponse> {
+    return await findExperienceById(experienceId);
+  }
+
+  @Mutation(() => CreateServiceResponse)
+  @UseMiddleware(isAuth)
+  async updateService(
+    @Arg("updateServiceInputs") updateServiceInputs: UpdateServiceInputs,
+    @Ctx() { req }: ApolloRedisContext
+  ): Promise<UpdateServiceResponse> {
+    // @ts-ignore
+    const { userId } = req.session;
+
+    return await updateService(updateServiceInputs, userId);
+  }
+
+  @Mutation(() => ServiceImageResponse)
+  async insertImageService(
+    @Arg("serviceId", () => Int) serviceId: number,
+    @Arg("urlImage", () => [String]) urlImage: string[]
+  ): Promise<ServiceImageResponse> {
+    try {
+      return await insertImage(serviceId, urlImage);
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    @Mutation(() => CreateServiceResponse)
-    @UseMiddleware(isAuth)
-    async createService(
-        @Arg('createServiceInputs') createServiceInputs: CreateServiceInputs,
-        @Ctx() {req}: ApolloRedisContext
-    ): Promise<CreateServiceResponse> {
-        // @ts-ignore
-        const {userId} = req.session;
-        // validate winery
-        return await createService(createServiceInputs, userId);
+  @Mutation(() => ServiceImageResponse)
+  async deleteImageService(
+    @Arg("serviceImageId", () => Int) serviceImageId: number
+  ): Promise<ServiceImageResponse> {
+    try {
+      return await deleteImage(serviceImageId);
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    @Query(() => FindExperienceResponse)
-    async findExperienceById(
-        @Arg('experienceId', () => Int) experienceId: number
-    ): Promise<FindExperienceResponse> {
-        return await findExperienceById(experienceId);
-    };
-
-    @Mutation(() => CreateServiceResponse)
-    @UseMiddleware(isAuth)
-    async updateService(
-        @Arg('updateServiceInputs') updateServiceInputs: UpdateServiceInputs,
-        @Ctx() {req}: ApolloRedisContext
-    ): Promise<UpdateServiceResponse> {
-        // @ts-ignore
-        const {userId} = req.session;
-        
-        return await updateService(updateServiceInputs, userId);
+  @Mutation(() => ServiceCoverImageChangeResponse)
+  async changeCoverPageImageService(
+    @Arg("serviceId", () => Int) serviceId: number,
+    @Arg("serviceImageId", () => Int) serviceImageId: number
+  ): Promise<ServiceCoverImageChangeResponse> {
+    try {
+      return await changeCoverPicture(serviceId, serviceImageId);
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    @Mutation(() => ServiceImageResponse)
-    async insertImageService(
-        @Arg('serviceId', () => Int) serviceId: number,
-        @Arg('urlImage', () => [String]) urlImage: string[],
-    ): Promise<ServiceImageResponse> {
-        try {            
-            return await insertImage(serviceId,urlImage)
-        } catch (error) {
-            throw new Error(error)
-        }
+  @Mutation(() => ServiceImageResponse)
+  async createDefaultImageToEvent(
+    @Arg("eventType", () => EventType) eventType: EventType,
+    @Arg("urlImage", () => String) urlImage: string
+  ): Promise<ServiceImageResponse> {
+    try {
+      return await createDefaultImageToEvent(eventType, urlImage);
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    @Mutation(() => ServiceImageResponse)
-    async deleteImageService(
-        @Arg('serviceImageId', () => Int) serviceImageId: number,
-    ): Promise<ServiceImageResponse> {
-        try {            
-            return await deleteImage(serviceImageId)
-        } catch (error) {
-            throw new Error(error)
-        }
+  @Mutation(() => ServiceImageResponse)
+  async updateDefaultImageToEvent(
+    @Arg("eventType", () => EventType) eventType: EventType,
+    @Arg("urlImage", () => String) urlImage: string
+  ): Promise<ServiceImageResponse> {
+    try {
+      return await updateDefaultImageToEvent(eventType, urlImage);
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    @Mutation(() => ServiceCoverImageChangeResponse)
-    async changeCoverPageImageService(
-        @Arg('serviceId', () => Int) serviceId: number,
-        @Arg('serviceImageId', () => Int) serviceImageId: number,
-    ): Promise<ServiceCoverImageChangeResponse> {
-        try {
-            return await changeCoverPicture(serviceId,serviceImageId)
-        } catch (error) {
-            throw new Error(error)
-        }
+  @Mutation(() => ServiceImageResponse)
+  async deleteDefaultImageToEvent(
+    @Arg("eventType", () => EventType) eventType: EventType
+  ): Promise<ServiceImageResponse> {
+    try {
+      return await deleteDefaultImageToEvent(eventType);
+    } catch (error) {
+      throw new Error(error);
     }
-
-    @Mutation(() => ServiceImageResponse)
-    async createDefaultImageToEvent(
-        @Arg('eventType', () => EventType) eventType: EventType,
-        @Arg('urlImage', () => String) urlImage: string,
-    ): Promise<ServiceImageResponse> {
-        try {            
-            return await createDefaultImageToEvent(eventType,urlImage)
-        } catch (error) {
-            throw new Error(error)
-        }
-    }
-
-    @Mutation(() => ServiceImageResponse)
-    async updateDefaultImageToEvent(
-        @Arg('eventType', () => EventType) eventType: EventType,
-        @Arg('urlImage', () => String) urlImage: string,
-    ): Promise<ServiceImageResponse> {
-        try {            
-            return await updateDefaultImageToEvent(eventType,urlImage)
-        } catch (error) {
-            throw new Error(error)
-        }
-    }
-
-    @Mutation(() => ServiceImageResponse)
-    async deleteDefaultImageToEvent(
-        @Arg('eventType', () => EventType) eventType: EventType,
-    ): Promise<ServiceImageResponse> {
-        try {            
-            return await deleteDefaultImageToEvent(eventType)
-        } catch (error) {
-            throw new Error(error)
-        }
-    }
+  }
 }
