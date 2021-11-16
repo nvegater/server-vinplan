ee41ba4893d9939988a8ee9d4a76e3bd38a48571
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2021-11-16 19:45:01 +0100

    add data services for Stripe

--- src/dataServices/payment.ts
+++ src/dataServices/payment.ts
@@ -16,3 +16,44 @@ export const createPaymentIntent_DS = async (
 ): Promise<Stripe.PaymentIntent> => {
   return await stripe.paymentIntents.create(params);
 };
+
+export const createIncompleteSubscription_DS = async (
+  params: Stripe.SubscriptionCreateParams
+) => {
+  return await stripe.subscriptions.create({
+    ...params,
+    payment_behavior: "default_incomplete",
+  });
+};
+
+/**
+ * Creates and invoice containing a payment_intent.
+ * Stripe.PaymentIntent TRacks Lifecyle of every payment
+ * */
+export const createPaymentIntentId_DS = async (
+  params: Stripe.InvoiceCreateParams
+) => {
+  const invoice = await stripe.invoices.create(params);
+  // return this to the Frontend to the checkout session.
+
+  // Stripe.PaymentIntent TRacks Lifecyle of every payment
+  // When a payment is due for a subscription, the invoice, and therefore the payment intend is created
+  const paymentIntent: Stripe.PaymentIntent | string | null =
+    invoice.payment_intent;
+
+  // TODO: Check if paymentIntent is null
+  return paymentIntent;
+};
+
+export const createCheckoutSession_DS = async (
+  params: Stripe.Checkout.SessionCreateParams
+) => {
+  return await stripe.checkout.sessions.create({ ...params });
+};
+
+export const confirmPaymentIntent_DS = async (
+  paymentIntentId: string,
+  params: Stripe.PaymentIntentConfirmParams
+) => {
+  return await stripe.paymentIntents.confirm(paymentIntentId, { ...params });
+};
