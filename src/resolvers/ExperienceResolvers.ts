166060dd943fa478a788dc4e4e28ef7ef994b8db
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2021-12-08 01:04:50 +0100

    add recurrence Rules for generateRecurrence. Is now generating proper recurrence Rules without any exception or custom Dates

--- src/resolvers/ExperienceResolvers.ts
+++ src/resolvers/ExperienceResolvers.ts
@@ -1,10 +1,34 @@
-import { Int, Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Query, Resolver } from "type-graphql";
 import { Experience } from "../entities/Experience";
+import { generateRecurrence } from "../useCases/experiences/recurrent/recurrenceRules";
 
+// default is ISO format ("isoDate") - "2018-02-07T21:04:39.573Z"
+// https://typegraphql.com/docs/0.16.0/scalars.html
+@InputType()
+export class CreateRecurrentDatesInputs {
+  @Field(() => Date)
+  startDate: Date;
+  @Field(() => Date)
+  endDate: Date;
+  @Field()
+  durationInMinutes: number;
+  @Field()
+  typeOfEvent: string;
+  @Field(() => [Date])
+  customDates: Date[];
+  @Field(() => [Date])
+  exceptions: Date[];
+  @Field(() => [String])
+  exceptionDays: string[];
+}
 @Resolver(Experience)
 export class ExperienceResolvers {
-  @Query(() => Int)
-  async allExperiences() {
-    return 0;
+  //@Authorized("owner")
+  @Query(() => [String])
+  recurrentDates(
+    @Arg("createRecurrentDatesInputs")
+    createRecurrentDatesInputs: CreateRecurrentDatesInputs
+  ): string[] {
+    return generateRecurrence({ ...createRecurrentDatesInputs });
   }
 }
---
+++ src/useCases/experiences/recurrent/recurrenceRules.ts
@@ -0,0 +1,66 @@
+import { Schedule } from "./rschedule";
+import moment from "moment";
+import { eachDayOfInterval } from "date-fns";
+import { IRuleOptions } from "@rschedule/core";
+
+//type WeekdayStr = "MO" | "TU" | "WE" | "TH" | "FR" | "SA" | "SU";
+//type TypeOfEvent = "One Time" | "Periodic" | "All day";
+// TODO write wrapper function to always use UTC
+
+interface CreateRecurrentDatesInputs {
+  startDate: Date;
+  endDate: Date;
+  durationInMinutes: number;
+  typeOfEvent: string;
+  customDates: Date[];
+  exceptions: Date[];
+  exceptionDays: string[];
+}
+
+export const generateRecurrence = ({
+  startDate,
+  endDate,
+  durationInMinutes,
+}: CreateRecurrentDatesInputs) => {
+  const allTheDays: Date[] = eachDayOfInterval({
+    start: moment(startDate).toDate(),
+    end: moment(endDate).toDate(),
+  });
+
+  const oneRulePerDay: IRuleOptions[] = allTheDays.map((date) => {
+    return {
+      frequency: "MINUTELY",
+      start: moment(date)
+        .hours(moment(startDate).hours())
+        .minutes(moment(startDate).minutes())
+        .seconds(0)
+        .milliseconds(0),
+      end: moment(date)
+        .hours(moment(endDate).hours())
+        .minutes(moment(endDate).minutes())
+        .seconds(0)
+        .milliseconds(0)
+        .add(durationInMinutes, "minutes"),
+      interval: durationInMinutes,
+    };
+  });
+
+  const schedule = new Schedule({
+    rrules: [...oneRulePerDay],
+  });
+
+  const array = schedule.occurrences().toArray();
+  const formattedArray = schedule
+    .occurrences()
+    .toArray()
+    .map((date) => moment(date.toISOString()).format());
+
+  const formattedUTCArray = schedule
+    .occurrences()
+    .toArray()
+    .map((date) => moment.utc(date.toISOString()).format());
+  console.log(array, formattedArray);
+  console.log(formattedUTCArray);
+
+  return [];
+};
