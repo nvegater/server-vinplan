62fad6ba34d2697d94cf65ad60f243202ab2468d
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2022-01-12 13:32:45 +0100

    add useless skeleton

--- src/resolvers/ReservationResolvers.ts
+++ src/resolvers/ReservationResolvers.ts
@@ -1,10 +1,16 @@
-import { Int, Query, Resolver } from "type-graphql";
+import { Int, Mutation, Query, Resolver } from "type-graphql";
 import { Reservation } from "../entities/Reservation";
+import { reservation } from "../useCases/reservations";
 
 @Resolver(Reservation)
 export class ReservationResolvers {
   @Query(() => Int)
   async allReservations() {
     return 0;
+  }
+
+  @Mutation(() => Boolean)
+  async reserveAsGuest() {
+    return reservation();
   }
 }
